9e5bf254f39b23c6082c58533b0f1527ce1e5bc0
IvanIhnatsiuk <ivan.ihnatsiuk@bankifi.com>
2025-12-17 19:40:02 +0100

    fix: revert changes in occurenceUtils

--- ios/utils/OccurenceUtils.h
+++ ios/utils/OccurenceUtils.h
@@ -1,93 +1,47 @@
 #pragma once
 #import "EnrichedTextInputView.h"
 #import "StylePair.h"
-#import <Foundation/Foundation.h>
 
 @interface OccurenceUtils : NSObject
 + (BOOL)detect:(NSAttributedStringKey _Nonnull)key
-         inString:(NSAttributedString *_Nonnull)string
-          inRange:(NSRange)range
-    withCondition:(BOOL(NS_NOESCAPE ^ _Nonnull)(id _Nullable value,
-                                                NSRange range))condition;
-
-+ (BOOL)detectMultiple:(NSArray<NSAttributedStringKey> *_Nonnull)keys
-              inString:(NSAttributedString *_Nonnull)string
-               inRange:(NSRange)range
-         withCondition:(BOOL(NS_NOESCAPE ^ _Nonnull)(id _Nullable value,
-                                                     NSRange range))condition;
-
-+ (BOOL)any:(NSAttributedStringKey _Nonnull)key
-         inString:(NSAttributedString *_Nonnull)string
-          inRange:(NSRange)range
-    withCondition:(BOOL(NS_NOESCAPE ^ _Nonnull)(id _Nullable value,
-                                                NSRange range))condition;
-
-+ (BOOL)anyMultiple:(NSArray<NSAttributedStringKey> *_Nonnull)keys
-           inString:(NSAttributedString *_Nonnull)string
-            inRange:(NSRange)range
-      withCondition:(BOOL(NS_NOESCAPE ^ _Nonnull)(id _Nullable value,
-                                                  NSRange range))condition;
-
-+ (NSArray<StylePair *> *_Nonnull)all:(NSAttributedStringKey _Nonnull)key
-                             inString:(NSAttributedString *_Nonnull)string
-                              inRange:(NSRange)range
-                        withCondition:(BOOL(NS_NOESCAPE ^
-                                            _Nonnull)(id _Nullable value,
-                                                      NSRange range))condition;
-
-+ (NSArray<StylePair *> *_Nonnull)
-      allMultiple:(NSArray<NSAttributedStringKey> *_Nonnull)keys
-         inString:(NSAttributedString *_Nonnull)string
-          inRange:(NSRange)range
-    withCondition:(BOOL(NS_NOESCAPE ^ _Nonnull)(id _Nullable value,
-                                                NSRange range))condition;
-+ (BOOL)detect:(NSAttributedStringKey _Nonnull)key
         withInput:(EnrichedTextInputView *_Nonnull)input
           inRange:(NSRange)range
     withCondition:(BOOL(NS_NOESCAPE ^ _Nonnull)(id _Nullable value,
                                                 NSRange range))condition;
-
 + (BOOL)detect:(NSAttributedStringKey _Nonnull)key
         withInput:(EnrichedTextInputView *_Nonnull)input
           atIndex:(NSUInteger)index
-    checkPrevious:(BOOL)checkPrev
+    checkPrevious:(BOOL)check
     withCondition:(BOOL(NS_NOESCAPE ^ _Nonnull)(id _Nullable value,
                                                 NSRange range))condition;
-
 + (BOOL)detectMultiple:(NSArray<NSAttributedStringKey> *_Nonnull)keys
              withInput:(EnrichedTextInputView *_Nonnull)input
                inRange:(NSRange)range
          withCondition:(BOOL(NS_NOESCAPE ^ _Nonnull)(id _Nullable value,
                                                      NSRange range))condition;
-
 + (BOOL)any:(NSAttributedStringKey _Nonnull)key
         withInput:(EnrichedTextInputView *_Nonnull)input
           inRange:(NSRange)range
     withCondition:(BOOL(NS_NOESCAPE ^ _Nonnull)(id _Nullable value,
                                                 NSRange range))condition;
-
 + (BOOL)anyMultiple:(NSArray<NSAttributedStringKey> *_Nonnull)keys
           withInput:(EnrichedTextInputView *_Nonnull)input
             inRange:(NSRange)range
       withCondition:(BOOL(NS_NOESCAPE ^ _Nonnull)(id _Nullable value,
                                                   NSRange range))condition;
-
-+ (NSArray<StylePair *> *_Nonnull)all:(NSAttributedStringKey _Nonnull)key
-                            withInput:(EnrichedTextInputView *_Nonnull)input
-                              inRange:(NSRange)range
-                        withCondition:(BOOL(NS_NOESCAPE ^
-                                            _Nonnull)(id _Nullable value,
-                                                      NSRange range))condition;
-
-+ (NSArray<StylePair *> *_Nonnull)
++ (NSArray<StylePair *> *_Nullable)all:(NSAttributedStringKey _Nonnull)key
+                             withInput:(EnrichedTextInputView *_Nonnull)input
+                               inRange:(NSRange)range
+                         withCondition:(BOOL(NS_NOESCAPE ^
+                                             _Nonnull)(id _Nullable value,
+                                                       NSRange range))condition;
++ (NSArray<StylePair *> *_Nullable)
       allMultiple:(NSArray<NSAttributedStringKey> *_Nonnull)keys
         withInput:(EnrichedTextInputView *_Nonnull)input
           inRange:(NSRange)range
     withCondition:(BOOL(NS_NOESCAPE ^ _Nonnull)(id _Nullable value,
                                                 NSRange range))condition;
-
 + (NSArray *_Nonnull)getRangesWithout:(NSArray<NSNumber *> *_Nonnull)types
                             withInput:(EnrichedTextInputView *_Nonnull)input
                               inRange:(NSRange)range;
-
 @end
