f53965b35704eb1fce5dc45956ff66c8414e51bf
IvanIhnatsiuk <ivan.ihnatsiuk@bankifi.com>
2025-12-29 22:04:38 +0100

    chore: build lib for v0.0.11

---
+++ lib/typescript/src/index.d.ts
@@ -0,0 +1,3 @@
+export * from './EnrichedTextInput';
+export type { OnChangeTextEvent, OnChangeHtmlEvent, OnChangeStateEvent, OnLinkDetected, OnMentionDetected, OnChangeSelectionEvent, OnChangeColorEvent, } from './EnrichedTextInputNativeComponent';
+//# sourceMappingURL=index.d.ts.map
