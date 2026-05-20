6a672b68af12203886cda7c16e8381dbdcd0a576
IvanIhnatsiuk <ivan.ihnatsiuk@bankifi.com>
2026-05-20 17:02:31 +0200

    fix: placeholder visibility when inserting text programmatically

--- ios/EnrichedTextInputView.mm
+++ ios/EnrichedTextInputView.mm
@@ -1079,6 +1079,7 @@ Class<RCTComponentViewProtocol> EnrichedTextInputViewCls(void) {
                        selectedRange:range];
 
   textView.selectedRange = NSMakeRange(range.location + insertedText.length, 0);
+  [self anyTextMayHaveBeenModified];
 }
 
 - (NSArray<NSNumber *> *)getPresentStyleTypesFrom:(NSArray<NSNumber *> *)types
