7bea325510665f455ee661ee60dcb7028a6947e3
IvanIhnatsiuk <ivan.ihnatsiuk@bankifi.com>
2026-07-13 17:53:48 +0200

    fix: non editable paragraphs insertion on android

--- android/src/main/java/com/swmansion/enriched/utils/SpannableUtils.kt
+++ android/src/main/java/com/swmansion/enriched/utils/SpannableUtils.kt
@@ -1,6 +1,7 @@
 package com.swmansion.enriched.utils
 
 import android.text.Spannable
+import com.swmansion.enriched.spans.interfaces.EnrichedNonEditableParagraphSpan
 import com.swmansion.enriched.spans.interfaces.EnrichedParagraphSpan
 import com.swmansion.enriched.spans.interfaces.EnrichedSpan
 
